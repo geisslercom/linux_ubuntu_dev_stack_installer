043067e04e972ab13b6775b31aebb0413fba09b3
Lars Goericke <larsgoericke@gmail.com>
2015-08-15 22:54:36 +0200

    war ich nicht

--- sub/installBin.sh
+++ sub/installBin.sh
@@ -5,10 +5,10 @@ cd bin
 
 NODE_VERSION="0.12.7"
 wget https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION-linux-x64.tar.gz
-tar -xf node-v$NODE_VERSION-linux-x64.tar.gz 
-exit
-mv node-v$NODE_VERSION-linux-x64 /usr/local
+tar -xf node-v$NODE_VERSION-linux-x64.tar.gz
+sudo mv node-v$NODE_VERSION-linux-x64 /usr/local
 echo 'export PATH="/usr/local/node-v$NODE_VERSION-linux-x64:$PATH"' >> ~/.zshrc  
+rm node-v$NODE_VERSION-linux-x64.tar.gz
 
 TESTDISK=testdisk-7.0.linux26-x86_64.tar.bz2
 wget http://www.cgsecurity.org/$TESTDISK
@@ -16,6 +16,6 @@ tar -xf $TESTDISK
 sudo mv testdisk-7.0 /usr/local/
 echo 'export PATH="/usr/local/testdisk-7.0:$PATH"' >> ~/.zshrc  
 echo "Testdisk installed"
-
+rm $TESTDISK
 
 cd ..

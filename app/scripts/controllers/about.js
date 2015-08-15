4a40a28e78bb287a46452e96667f13317cdb09f2
Lars Goericke <larsgoericke@gmail.com>
2015-08-15 23:21:45 +0200

    yo installer

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name linuxUbuntuDevStackInstallerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the linuxUbuntuDevStackInstallerApp
+ */
+angular.module('linuxUbuntuDevStackInstallerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name linuxUbuntuDevStackInstallerApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the linuxUbuntuDevStackInstallerApp
+ */
+angular.module('linuxUbuntuDevStackInstallerApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
